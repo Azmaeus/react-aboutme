0da6b7711320f69c147fb4ad27d41030720d0c49
Eric F Olsen <eric@ericolsen.us>
2023-03-06 11:45:38 -0700

    Break-out App JSX into separate functions

--- src/App.js
+++ src/App.js
@@ -4,15 +4,57 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Eric F. Olsen
-        </h1>
-        <h3>
-          Software Engineer
-        </h3>
-      </header>
+      <Header />
+      {/* <Story />
+      <Resume />
+      <Projects />
+      <Testimonials /> */}
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>
+        Eric F. Olsen
+      </h1>
+      <h3>
+        Software Engineer
+      </h3>
+    </header>
+  );
+}
+
+function Story() {
+  return (
+    <div className="section">
+      <h2>my story goes here!</h2>
+    </div>
+  );
+}
+
+function Resume() {
+  return (
+    <div className="section">
+      <h2>resume goes here!</h2>
+    </div>
+  );
+}
+
+function Projects() {
+  return (
+    <div className="section">
+      <h2>projects go here!</h2>
+    </div>
+  );
+}
+
+function Testimonials() {
+  return (
+    <div className="section">
+      <h2>testimonials go here!</h2>
     </div>
   );
 }
