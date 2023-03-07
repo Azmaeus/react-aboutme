577db09d4bef1dce9cb72d7278bb936524e4ae36
Eric F Olsen <eric@ericolsen.us>
2023-03-07 16:41:43 -0700

    Add resume component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './headshot.jpg';
 import './App.css';
+import Resume from './components/Resume';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           Software Engineer
         </h3>
       </header>
+      <Resume />
     </div>
   );
 }
---
+++ src/components/Resume.js
@@ -0,0 +1,9 @@
+function Resume () {
+  return (
+    <div className="section">
+      <h2>resume goes here!</h2>
+    </div>
+  );
+}
+
+export default Resume;
