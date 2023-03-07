74e22cd6ce96561eab80fc99b6553d804e9b5e53
Eric F Olsen <eric@ericolsen.us>
2023-03-07 16:42:58 -0700

    Add projects component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './headshot.jpg';
 import './App.css';
+import Projects from './components/Projects';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           Software Engineer
         </h3>
       </header>
+      <Projects />
     </div>
   );
 }
