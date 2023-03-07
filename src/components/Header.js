2b96ec08c28777b4fc52903010cc04bfef2fe158
Eric F Olsen <eric@ericolsen.us>
2023-03-07 16:59:06 -0700

    Remove underscore from img alt

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import profile_photo from '../profile-photo.jpg';
 function Header () {
   return (
     <header className="App-header">
-      <img src={profile_photo} className="App-profile-photo" alt="profile_photo" />
+      <img src={profile_photo} className="App-profile-photo" alt="profile photo" />
         <h1>Eric F. Olsen</h1>
         <h3>Software Engineer</h3>
       </header>
