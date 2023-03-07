9a41d4bb4a3d17ffb5da8d17e58533bea9d4430f
Eric F Olsen <eric@ericolsen.us>
2023-03-07 16:28:18 -0700

    Move header into its own file, rename headshot to profile photo

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import profile_photo from '../profile-photo.jpg';
+
+function Header () {
+  return (
+    <header className="App-header">
+      <img src={profile_photo} className="App-profile-photo" alt="profile_photo" />
+        <h1>Eric F. Olsen</h1>
+        <h3>Software Engineer</h3>
+      </header>
+  );
+}
+
+export default Header;
