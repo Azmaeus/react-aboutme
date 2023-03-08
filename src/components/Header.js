cdef39fb8eb87d844d8a6efca4b8eb091824fd6a
Eric F Olsen <eric@ericolsen.us>
2023-03-07 17:36:06 -0700

    Update header to avoid skipping heading levels
    For accessibility, avoid using heading elements (e.g. h1, h2, etc) for anything other than headings (new sections) because assistive technology users might think they missed something.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header () {
     <header className="App-header">
       <img src={profile_photo} className="App-profile-photo" alt="profile photo" />
         <h1>Eric F. Olsen</h1>
-        <h3>Software Engineer</h3>
+        <p class="App-header-occupation">Software Engineer</p>
       </header>
   );
 }
