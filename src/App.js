a4f2454c599e5f56600d7f711680686936564e0a
Eric F Olsen <eric@ericolsen.us>
2023-03-08 07:52:58 -0800

    Add section framework (#2)
    * Move background and font color from Header to App
    
    * Create section class
    
    * Break-out App JSX into separate functions
    
    * Set vscode default tab size
    
    The auto-detect was setting new files to 4 spaces, but the create-react-app boilerplate was using 2 spaces.
    
    * Move header into its own file, rename headshot to profile photo
    
    * Remove underscore from img alt
    
    * Update header to avoid skipping heading levels
    
    For accessibility, avoid using heading elements (e.g. h1, h2, etc) for anything other than headings (new sections) because assistive technology users might think they missed something.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './headshot.jpg';
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Eric F. Olsen
-        </h1>
-        <h3>
-          Software Engineer
-        </h3>
-      </header>
+      <Header />
     </div>
   );
 }
