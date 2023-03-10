de185bb82be16950cb769e88e0058bbebb5af196
Eric F Olsen <eric@ericolsen.us>
2023-03-10 13:01:34 -0700

    Add story contents

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,8 +2,10 @@ import './Story.css';
 
 function Story() {
   return (
-      <h2>my story goes here!</h2>
     <section id="story" className="section App-story">
+      <p>I am an experienced and innovative Software Engineer with a passion for problem-solving and creating efficient solutions. My passion for technology began when I was young, playing on my father's business computers after-hours. After a brief stint in college, I jumped into the workforce.</p>
+      <p>During my decade as a Technical Support Engineer I honed my communication skills, learned a wide range of technologies, and discovered I had a talent for creating script-based solutions so I turned my focus to Software Engineering.</p>
+      <p>I've since worked in companies both big and small, and experienced a lot. I am ready to pivot when necessary, and able to quickly pick-up whatever knowledge is necessary for the task-at-hand. My expertise in solving complex problems and creating innovative solutions would make me a valuable asset to any technical team.</p>
     </section>
   );
 }
